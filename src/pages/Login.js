b747b5dae5c6ca52e00dc164bc45f298c05fa4ad
sohel-gitlab <tosohel22@gmail.com>
2021-05-05 15:54:12 +0600

    test update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,7 +10,7 @@ const Login = () => {
     <div className="container">
       <img src={loginImg} alt="github-user" />
       <h1>github user</h1>
-      <button className="btn" onClick={loginWithRedirect}>login / signup</button>
+      <button className="btn" onClick={loginWithRedirect}>login / signup...</button>
     </div>
   </Wrapper>;
 };
